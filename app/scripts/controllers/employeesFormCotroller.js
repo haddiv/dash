e124b7640f7e74c86c65ec85ffc8b3eab7601089
asthik <astx.kras@gmail.com>
2017-09-23 09:23:28 +0500

    add function

--- app/scripts/controllers/employeesFormCotroller.js
+++ app/scripts/controllers/employeesFormCotroller.js
@@ -20,4 +20,8 @@ angular.module('sbAdminApp')
         $scope.days=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,30, 31];
         $scope.months=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'];
         $scope.years = YearArr;
-    });
+        $scope.clickToClone = function(sourceId, cloneId) {
+            var sourceHtml = angular.element(document.getElementById(sourceId)).html();
+            angular.element(document.getElementById(cloneId)).append(sourceHtml);
+        }
+    })
