3b9a87459ceb5cb251b0d027fc5f16051e61495f
HGohar <goharhovhannisyan19@gmail.com>
2017-09-27 02:43:57 +0400

    node server

--- app/node/server.js
+++ app/node/server.js
@@ -1,12 +1,62 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 //create a server object:
 http.createServer(function (req, res) {
-    console.log("ab")
-    var q = url.parse(req.url, true);
-    console.log(q.pathname);
-    if(q.pathname=="/create"){
-console.log("a")
-    }
+   console.log('datas')
+   
+     var q = url.parse(req.url, true);
+    var datas = q.query;
+	console.log(datas)
+	//console.log(datas);
+     if(q.pathname=="/create"){
+	// console.log(req.url)
+			 fs.readFile('../data/employees.json', 'utf8', function (err,data){
+			   if(err){
+			   //console.log(err)
+			   }
+			  else{
+			  
+			  var jsonArray= [];
+				 if(data==""){
+				 
+					jsonArray.push(datas);
+					var employee_json = JSON.stringify(jsonArray);
+					fs.writeFile('../data/employees.json', employee_json);
+					//console.log(employee_json)
+					
+						}
+						else{
+						var jsonArray= [];
+						fs.readFile('../data/employees.json', 'utf8', function (err,data){
+							
+							var intoJsonObj = JSON.parse(data);
+							//var str = 
+							console.log(intoJsonObj);
+							
+						
+							for(var i=0;i<intoJsonObj.length; i++){
+									jsonArray.push(intoJsonObj[i])
+									console.log(intoJsonObj[i])
+									}
+									
+									
+									jsonArray.push(datas);
+									var employee_json = JSON.stringify(jsonArray);
+									fs.writeFile('../data/employees.json', employee_json);
+									//console.log(employee_json)
+							
+							/* employee_json = JSON.stringify(jsonArray);
+							fs.appendFile('../data/employees.json', employee_json , function (err) {
+							  if (err) throw err;
+							  
+								
+								}) */
+								})
+							}
+  }; 			   }) 
+			
+    }  
    // console.log(q)
-}).listen(8080);
+}).listen(1337);
+
