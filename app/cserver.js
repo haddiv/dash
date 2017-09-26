74187ced8956ee33501f33a82aa4c21a3362122d
ManLe21 <manik_minasyan15@alumni.aua.am>
2017-09-27 02:43:22 +0400

    new files

---
+++ app/cserver.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+
+    console.log(q.pathname);
+    if(q.pathname == "/create"){
+        console.log("done!");
+        fs.readFile('./employees.json', 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(data);
+            if(data==''){
+                var employee = {name:'name',username:'test'};
+                var all_data = [];
+                all_data.push(employee);
+                var employee_json= JSON.stringify(all_data);
+                fs.writeFile('./employees.json', employees_json);
+                console.log(employees_json);
+            }
+        });
+    }
+
+}).listen(8080);
