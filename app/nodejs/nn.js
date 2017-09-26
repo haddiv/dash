ee4dd6fc56dc6c0c45799c93c5f137410c75b2dc
Misho91 <misha.avetisyan.1991@mail.ru>
2017-09-27 00:28:15 +0400

    form

--- app/nodejs/nn.js
+++ app/nodejs/nn.js
@@ -8,8 +8,8 @@ fs.readFile('./users.json', 'utf-8', function(err, data) {
     if (err) throw err;
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/html'});
-            var q = url.parse(req.url, true).query;
-            //
+            var a = url.parse(req.url, true);
+        var q = url.parse(req.url, true).query;
             // console.log(q);
 
         var arrayOfObjects = JSON.parse(data);
@@ -17,13 +17,13 @@ fs.readFile('./users.json', 'utf-8', function(err, data) {
         arrayOfObjects.users.push(q);
         var txt = JSON.stringify(arrayOfObjects);
         console.log(txt);
+       if(a.pathname=="/create/") {
+           fs.writeFile('./users.json', txt, function (err) {
+               if (err) throw err;
+               console.log('Done!')
+           });
 
-        fs.appendFile('./users.json', txt , function (err) {
-            if (err) throw err;
-            console.log('Done!')
-        });
-
-        res.end();
+       }  res.end();
     }).listen(8080);
 
 })
