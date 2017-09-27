0cc851a9f66c69e2affaebbd563ce0f076f7e673
asthik <astx.kras@gmail.com>
2017-09-27 08:33:03 +0500

    code is woring

--- app/views/server.js
+++ app/views/server.js
@@ -1,28 +1,52 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-
+//create a server object:
 http.createServer(function (req, res) {
-    console.log("Asti");
-    var q = url.parse(req.url, true).query;
-    console.log(datas);
-    console.log(q.pathname);
-    if(q.pathname == "/create"){
-        console.log("done!");
-        fs.readFile('../data/employees.json', 'utf8', function (err,data) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log(data);
-            if(data==''){
-                var employee = {name:'poxos',username:'test'};
-                var all_data = [];
-                all_data.push(employee);
-                var employee_json= JSON.stringify(all_data);
-                fs.writeFile('../data/employees.json', employee_json);
-                console.log(employee_json);
-            }
-        });
-    }
+    console.log('datas')
 
+    var q = url.parse(req.url, true);
+    var datas = q.query;
+    console.log(datas)
+    if (q.pathname == "/create") {
+        // console.log(req.url)
+        fs.readFile('../data/employees.json', 'utf8', function (err, data) {
+
+            if (!err) {
+
+                var jsonArray = [];
+                if (data == "") {
+
+                    jsonArray.push(datas);
+                    var employee_json = JSON.stringify(jsonArray);
+                    fs.writeFile('../data/employees.json', employee_json);
+                    console.log(employee_json)
+
+                }
+                else {
+                    var jsonArray = [];
+                    fs.readFile('../data/employees.json', 'utf8', function (err, data) {
+
+                        var intoJsonObj = JSON.parse(data);
+
+                        console.log(intoJsonObj);
+
+
+                        for (var i = 0; i < intoJsonObj.length; i++) {
+                            jsonArray.push(intoJsonObj[i])
+                            console.log(intoJsonObj[i])
+                        }
+
+
+                        jsonArray.push(datas);
+                        var employee_json = JSON.stringify(jsonArray);
+                        fs.writeFile('../data/employees.json', employee_json);
+
+                    })
+                }
+            };
+        })
+
+    }
+    // console.log(q)
 }).listen(8080);
