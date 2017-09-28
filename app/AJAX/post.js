2f86d189127870b2e712966f4cfd566b60bd1e4a
Your Name <ashotgharakeshishyan@gmail.com>
2017-09-28 15:04:28 +0400

    ajax

--- app/AJAX/post.js
+++ app/AJAX/post.js
@@ -3,7 +3,7 @@ app.controller('LoginCtrl', function ($scope, $http) {
     $scope.user = {};
     $scope.user.customField = 'customValue';
     $scope.submitForm = function (form) {
-        $http.post('form.html', $scope.user)
+        $http.post('/localhost:8080/', $scope.user)
             .success(function (data) {
                 console.log('data:', data);
             })
